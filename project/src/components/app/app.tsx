5cf7fa897cf29fc553af1dc994a488f360e493a6
e1ect <e1ectt96@gmail.com>
2022-03-05 21:41:08 +0300

    3.2 Добавляет маршрутизацию

--- project/src/components/app/app.tsx
+++ project/src/components/app/app.tsx
@@ -1,4 +1,9 @@
+import {Route, BrowserRouter, Routes} from 'react-router-dom';
+import {AppRoute} from '../../consts';
 import Main from '../main/main';
+import Favorites from '../favorites/favorites';
+import Login from '../login/login';
+import Room from '../room/room';
 
 type AppProps = {
   placesCount: number;
@@ -6,7 +11,26 @@ type AppProps = {
 
 function App({placesCount}: AppProps): JSX.Element {
   return (
-    <Main placesCount={placesCount} />
+    <BrowserRouter>
+      <Routes>
+        <Route
+          path = {AppRoute.Main}
+          element={<Main placesCount={placesCount} />}
+        />
+        <Route
+          path={ AppRoute.Favorites}
+          element={<Favorites/>}
+        />
+        <Route
+          path={AppRoute.Login}
+          element={<Login />}
+        />
+        <Route
+          path={AppRoute.Room}
+          element={<Room/>}
+        />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
