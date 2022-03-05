4f2fee9a344afab9bc92679d237d8b359aac60f9
e1ect <e1ectt96@gmail.com>
2022-03-05 22:57:08 +0300

    3.5 Заменит Route на PrivateRoute.

--- project/src/components/app/app.tsx
+++ project/src/components/app/app.tsx
@@ -1,6 +1,7 @@
-import {Route, BrowserRouter, Routes} from 'react-router-dom';
-import {AppRoute} from '../../consts';
+import {BrowserRouter, Route, Routes} from 'react-router-dom';
+import {AppRoute, AuthorizationStatus} from '../../consts';
 import Main from '../main/main';
+import PrivateRoute from '../private-route/private-route';
 import Favorites from '../favorites/favorites';
 import Login from '../login/login';
 import Room from '../room/room';
@@ -20,7 +21,11 @@ function App({placesCount}: AppProps): JSX.Element {
         />
         <Route
           path={ AppRoute.Favorites}
-          element={<Favorites/>}
+          element={
+            <PrivateRoute authorizationStatus={AuthorizationStatus.NoAuth}>
+              <Favorites />
+            </PrivateRoute>
+          }
         />
         <Route
           path={AppRoute.Login}
