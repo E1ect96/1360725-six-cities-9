0a0071a113b38c3e58a6c719f98c9a470de0133a
e1ect <e1ectt96@gmail.com>
2022-03-24 18:46:03 +0300

    6.1 добавляет редьюсер

---
+++ project/src/store/reducer.ts
@@ -0,0 +1,20 @@
+import {createReducer} from '@reduxjs/toolkit';
+import {CityName} from '../consts';
+import {offers} from '../mocks/offers';
+
+const initialState = {
+  currentCity: CityName.Amsterdam,
+  offers: offers,
+}
+
+const reducer = createReducer(initialState, (builder) => {
+  builder
+    .addCase(changeCity, (state, action) => {
+      state.currentCity = action.payload;
+    })
+    .addCase(fillOffersList, (state, action) => {
+      state.offers = action.payload;
+    });
+});
+
+export {reducer};
